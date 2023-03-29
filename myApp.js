4c808f62cf51f165932659c78750760a08ce8d1f
EBRIMA SONKO <ebrimasonko@EBRIMAs-MacBook-Air.local>
2023-03-29 02:44:06 +0200

    added

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 // your code here that uses mongoose
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
 
 let Person;
